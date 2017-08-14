bb0c77bf4dd395dad7afbbe5dcb7ea3ac3794d47
Gerd Heber <gheber@hdfgroup.org>
2017-08-14 12:17:57 +0000

    Prints the next JOBUID.

---
+++ jobuid.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+You can run this example like this:
+    .. code:: console
+            $ python jobuid.py
+"""
+import mysql.connector
+
+from config import MySQLDBConfig
+
+def get_next_jobuid():
+    """ returns the next JOBUID """
+
+    sql = "select max(uid)+1 as max_uid from processJob;"
+    db = mysql.connector.connect(host=MySQLDBConfig().prd_host,
+                                 user=MySQLDBConfig().prd_user,
+                                 passwd=MySQLDBConfig().prd_pass,
+                                 db=MySQLDBConfig().prd_schema)    
+    cur = db.cursor()
+    cur.execute(sql)
+    row = cur.fetchone()
+    db.close()
+    if len(row) == 1:
+        return row[0]
+    else:
+        return 1
+
+
+if __name__ == "__main__":
+    print(get_next_jobuid())
