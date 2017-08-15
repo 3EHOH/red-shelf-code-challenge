485de9453f3329d6f1b9a17ae4c0fd1e92027bc3
Gerd Heber <gheber@hdfgroup.org>
2017-08-15 17:19:03 +0000

    Added IEVA and PAC Super Groups.

---
+++ ieva.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import luigi
+import mysql.connector
+import subprocess
+from subprocess import Popen, PIPE
+
+from config import ModelConfig, MySQLDBConfig, PathConfig
+from run_55 import Run55 
+from hci3_reliability_analysis import HCI3ReliabilityAnalysis
+
+STEP = 'ieva'
+
+DB = ModelConfig().jobname + ModelConfig().rundate
+
+SQL_FILE = os.path.join(PathConfig().postec_path,
+                        'Master_IEVA_W_Description.sql')
+
+class IEVA(luigi.Task):
+    """ perform intra-episode variability analysis """
+    datafile = luigi.Parameter(default=STEP)
+    jobuid = luigi.IntParameter(default=-1)
+
+    def requires(self):
+        return [HCI3ReliabilityAnalysis(jobuid=self.jobuid)]
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(PathConfig().target_path,
+                                              self.datafile))
+
+    def run(self):
+        command = ['mysql', '-f', '-h{}'.format(MySQLDBConfig().prd_host),
+                   '--database={}'.format(DB),
+                   '-u{}'.format(MySQLDBConfig().prd_user),
+                   '-p{}'.format(MySQLDBConfig().prd_pass)]
+        with open(SQL_FILE) as input_file:
+            proc = subprocess.Popen(
+                command, stdin = input_file, stderr=PIPE, stdout=PIPE)
+            output,error = proc.communicate()
+            with self.output().open('w') as out:
+                out.write(str(error))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('ieva.py <JOBUID>')
+        exit(-1)
+    luigi.run([
+        'IEVA', 
+        '--workers', '1',
+        '--jobuid', sys.argv[1],
+        '--local-scheduler'])
---
+++ pac_super_groups.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import luigi
+import mysql.connector
+import subprocess
+from subprocess import Popen, PIPE
+
+from config import ModelConfig, MySQLDBConfig, PathConfig
+from run_55 import Run55 
+from ieva import IEVA
+
+STEP = 'pac_super_groups'
+
+DB = ModelConfig().jobname + ModelConfig().rundate
+
+SQL_FILE = os.path.join(PathConfig().postec_path,
+                        'pac_super_groups.sql')
+
+class PACSuperGroups(luigi.Task):
+    """ consolidate PAC code groups into PAC Super Groups """
+    datafile = luigi.Parameter(default=STEP)
+    jobuid = luigi.IntParameter(default=-1)
+
+    def requires(self):
+        return [IEVA(jobuid=self.jobuid)]
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(PathConfig().target_path,
+                                              self.datafile))
+
+    def run(self):
+        command = ['mysql', '-f', '-h{}'.format(MySQLDBConfig().prd_host),
+                   '--database={}'.format(DB),
+                   '-u{}'.format(MySQLDBConfig().prd_user),
+                   '-p{}'.format(MySQLDBConfig().prd_pass)]
+        with open(SQL_FILE) as input_file:
+            proc = subprocess.Popen(
+                command, stdin = input_file, stderr=PIPE, stdout=PIPE)
+            output,error = proc.communicate()
+            with self.output().open('w') as out:
+                out.write(str(error))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('pac_super_groups.py <JOBUID>')
+        exit(-1)
+    luigi.run([
+        'PACSuperGroups', 
+        '--workers', '1',
+        '--jobuid', sys.argv[1],
+        '--local-scheduler'])
