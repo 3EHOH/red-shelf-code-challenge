4215cfc82217111839a49849cf5cf31fc619893a
Gerd Heber <gheber@hdfgroup.org>
2017-08-21 19:44:31 +0000

    Modify the LD_LIBRARY_PATH for libjvm.so.

--- doit.sh
+++ doit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=/usr/lib/jvm/java/jre/lib/amd64/server/:$LD_LIBRARY_PATH
+
 # adjust the number of workers depending on your count settings in luigi.cfg
 # in NormanConfig and ConnieConfig
 
