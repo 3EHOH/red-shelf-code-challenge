e8cd2481919c0e723f4dcaeddf595bb192b9481e
Andrew Weinrich <andrew.weinrich@hdfgroup.org>
2017-10-08 23:25:36 +0000

    Split AWS config into separate file

---
+++ automation/example_aws_config.sh
@@ -0,0 +1,40 @@
+##########################################
+#
+# START AWS-SPECIFIC CONFIGURATION
+#
+##########################################
+
+# This section contains configuration variables that 
+# will depend on your specific AWS setup, such as
+# IP addresses and security group IDs. These must be
+# be specified manually.
+
+# SFTP/SCP instance parameters
+SCP_SERVER=172.1.1.1
+
+# AMIs for servers
+ROOT_AMI_ID="ami-abcdef12"
+MYSQL_AMI_ID="ami-abcdef12"
+MONGO_AMI_ID="ami-abcdef12"
+NORMAN_AMI_ID="ami-abcdef12"
+CONNIE_AMI_ID="ami-abcdef12"
+
+# instance types that will be used for servers
+ROOT_INSTANCE_TYPE="r3.8xlarge"
+MYSQL_INSTANCE_TYPE="r3.8xlarge"
+MONGO_INSTANCE_TYPE="r3.8xlarge"
+NORMAN_INSTANCE_TYPE="r3.8xlarge"
+CONNIE_INSTANCE_TYPE="r3.8xlarge"
+
+# Security groups that servers will be in (space-separated)
+SECURITY_GROUPS="sg-abcdef12 sg-abcdef12"
+
+# internal subnet that servers will be placed in (must have a connection to external internet)
+SUBNET_ID="subnet-abcdef12"
+
+
+##########################################
+#
+# END AWS-SPECIFIC CONFIGURATION
+#
+##########################################
