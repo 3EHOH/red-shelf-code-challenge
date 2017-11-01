79db4121f574d31a9cc10b977e4b7266737a850e
Zenon Evans <zenon.evans@hdfgroup.org>
2017-11-01 14:22:22 -0500

    re-add line to doit?

--- luigi/doit.sh
+++ luigi/doit.sh
@@ -7,4 +7,6 @@ export LD_LIBRARY_PATH=/usr/lib/jvm/java/jre/lib/amd64/server/:$LD_LIBRARY_PATH
 
 source ~/.bashrc
 
+cd /home/ec2-user/payformance/luigi
+
 python -m luigi --local-scheduler --workers 10 --module pipeline PipelineTask
