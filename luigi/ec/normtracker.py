81203ed242fc0af1c3673f89e46a981c27e4a465
Zenon Evans <zenon.evans@hdfgroup.org>
2017-11-06 17:38:56 -0600

    fix errant call to config var

--- luigi/ec/normtracker.py
+++ luigi/ec/normtracker.py
@@ -26,22 +26,19 @@ class NormTracker(luigi.Task):
     def track_norman():
         select_database = "use ecr;"
         update_status(select_database)
+
         drop_view = "drop view if exists normview;"
         update_status(drop_view)
-
-        # creating view or ecr table named normview for reference
 
         create_view = "create view normview as select j.client_id, j.jobName , j.uid as jobUid, s.uid, s.sequence, s.stepName, s.description, s.status, s.updated from processJob j join processJobStep s on s.jobUid = j.uid and j.client_id = 'Test' order by j.uid, s.sequence asc;"
         update_status(create_view)
 
         check_is_complete = "select exists(select * from normview where stepName='normalization' and status='Complete');"
-
         is_complete = get_status(check_is_complete)
 
         while not is_complete:
 
             query_complete = "select count(*) from jobStepQueue where jobUid=1 and stepName ='normalization' and status='Complete';"
-
             complete_count = get_status(query_complete)
 
             count = get_status("select count(*) from jobStepQueue where jobUid=1 and stepName ='normalization';")
@@ -51,8 +48,7 @@ class NormTracker(luigi.Task):
                 return True
                 break
             else:
-                # till norm completes show the current count
-                print("Current Normalization count-> ", complete_count)
+                print("Current normalization count: ", complete_count)
 
 
 if __name__ == "__main__":
