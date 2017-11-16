303d635f718a69d83047d915a9a920ce1696e4ae
andrew.weinrich <andrew.weinrich@hdfgroup.org>
2017-11-16 16:25:26 +0000

    Added toggle to normlauncher.py

--- luigi/ec/normlauncher.py
+++ luigi/ec/normlauncher.py
@@ -2,8 +2,9 @@ import boto3
 import luigi
 import os
 import time
+import subprocess
 
-from config import PathConfig, ModelConfig, MySQLDBConfig,  NormanConfig, MongoDBConfig
+from config import PathConfig, ModelConfig, MySQLDBConfig,  NormanConfig, MongoDBConfig, AwsConfig
 from ec.postmapreport import PostMapReport
 from run_55 import Run55
 
@@ -15,17 +16,17 @@ class NormLauncher(luigi.Task):
     """ launch external normalization services """
     datafile = luigi.Parameter(default=STEP)
     jobuid = luigi.IntParameter()
-
+    
     def requires(self):
         return [PostMapReport(jobuid=self.jobuid)]
-
+    
     def output(self):
         return luigi.LocalTarget(os.path.join(PathConfig().target_path,
                                               self.datafile))
-
+    
     @staticmethod
     def format_security_groups():
-
+        
         root_security_groups = os.getenv('ROOT_SECURITY_GROUPS')
         mysql_security_groups = os.getenv('MYSQL_SECURITY_GROUPS')
         mongo_security_groups = os.getenv('MONGO_SECURITY_GROUPS')
@@ -39,12 +40,28 @@ class NormLauncher(luigi.Task):
 
         return list(set(security_groups_formatted))
 
+
+    @staticmethod
+    def create_norman_command():
+        """Creates a norman invocation command"""
+        norman_command = "cd {luigidir}/; java -d64 -Xms8G -Xmx48G -cp {cpath} " \
+                         "-Dlog4j.configuration=file:/ecrfiles/scripts/log4jNorman.properties " \
+                         "control.NormalizationDriver configfolder={configfolder} " \
+                         "chunksize={chunksize} stopafter={stopafter}"
+
+        return norman_command.format(
+            luigidir=os.getenv('LUIGI_DIR'),
+            cpath=Run55.cpath(),
+            configfolder=ModelConfig().configfolder,
+            chunksize=NormanConfig().chunksize,
+            stopafter=NormanConfig().stopafter)
+
     @staticmethod
     def create_user_data_script():
         mongo_host = os.getenv('MONGO_HOST')
         mysql_host = os.getenv('MYSQL_HOST')
         luigi_dir = os.getenv('LUIGI_DIR')
-
+        
         user_data_host_info = """#!/bin/bash
         sed -i "s/md1.host=.*/md1.host={mongohost}/" {luigidir}/database.properties
         sed -i "s/prd.host=.*/prd.host={prdhost}/" {luigidir}/database.properties
@@ -56,19 +73,8 @@ class NormLauncher(luigi.Task):
         sed -i "s/template.host=.*/template.host={templatehost}/" {luigidir}/database.properties
         sed -i "s/template.user=.*/template.user={mysqluser}/" {luigidir}/database.properties
         sed -i "s/template.password=.*/template.password={mysqlpass}/" {luigidir}/database.properties"""
-
-        user_data_norm_command = ""
-
-        for _ in range(NormanConfig().processes_per_instance):
-            user_data_norm_command = user_data_norm_command + '\n' + \
-                                     "cd {luigidir}/; java -d64 -Xms8G -Xmx48G -cp {cpath} " \
-                                     "-Dlog4j.configuration=file:/ecrfiles/scripts/log4jNorman.properties " \
-                                     "control.NormalizationDriver configfolder={configfolder} " \
-                                     "chunksize={chunksize} stopafter={stopafter} &"
-
-        user_data_script = user_data_host_info + user_data_norm_command
-
-        return user_data_script.format(
+        
+        user_data_host_info = user_data_host_info.format(
             mongohost=mongo_host,
             luigidir=luigi_dir,
             prdhost=mysql_host,
@@ -76,37 +82,44 @@ class NormLauncher(luigi.Task):
             templatehost=mysql_host,
             epbhost=mysql_host,
             mysqluser=MySQLDBConfig().prd_user,
-            mysqlpass=MySQLDBConfig().prd_pass,
-            cpath=Run55.cpath(),
-            configfolder=ModelConfig().configfolder,
-            chunksize=NormanConfig().chunksize,
-            stopafter=NormanConfig().stopafter)
-
+            mysqlpass=MySQLDBConfig().prd_pass)
+        
+        user_data_norm_command = ""
+        
+        for _ in range(NormanConfig().processes_per_instance):
+            user_data_norm_command = \
+                user_data_norm_command + "\n " + create_norman_command() + " &"
+                
+        user_data_script = user_data_host_info + user_data_norm_command
+        
+        return user_data_script
+        
     @staticmethod
     def name_norms(norm_instances):
-
+        
         norm_names = []
-
+        
         host_name = os.getenv('HOSTNAME')
-
+        
         for instance in norm_instances:
             tag_name = host_name + 'Norm_' + instance.id
             ec2.create_tags(Resources=[instance.id], Tags=[{'Key': 'Name', 'Value': tag_name}])
             norm_names.append(tag_name)
-
+            
         return norm_names
-
-    def run(self):
-
+        
+    
+    
+    def run_distributed():
         norm_ami_id = os.getenv('NORMAN_AMI_ID')
         norm_instance_type = os.getenv('NORMAN_INSTANCE_TYPE')
         key_name = os.getenv('KEY_PAIR')
-
+        
         security_groups_formatted = self.format_security_groups()
         user_data_script = self.create_user_data_script()
-
+        
         print("USER DATA SCRIPT NORM LAUNCHER: ", user_data_script) #sanity check
-
+        
         norm_instances = ec2.create_instances(
             MinCount=1,
             MaxCount=NormanConfig().instance_count,
@@ -116,23 +129,38 @@ class NormLauncher(luigi.Task):
             SecurityGroups=security_groups_formatted,
             UserData=user_data_script
         )
-
+        
         time.sleep(30) #time buffer before iterating over and adding name tags
-
+        
         norm_names = self.name_norms(norm_instances)
-
+        
         time.sleep(60) #buffer to let instances reach running state before ending this step
-
+        
         running_instance_count = len(list(ec2.instances.filter(Filters=[{'Name': 'instance-state-name',
                                                                          'Values': ['running']},
                                                                         {'Name': 'tag:Name',
                                                                          'Values': norm_names}])))
-
+        
         if not running_instance_count == NormanConfig().instance_count:
             ValueError("Error: Norm instances not all running. Shutting down.")
+            
+        
+    def run_standalone(self):
+        total_norman_count = NormanConfig().instance_count * NormanConfig().processes_per_instance
+        
+        for _ in range(total_norman_count):
+            norman_command = create_norman_command()
+            subprocess.Popen(norman_command.split())
+            
+        
+    def run(self):
+        if AwsConfig().enable_distributed_mode:
+            self.run_distributed()
         else:
-            self.output().open('w').close()
-
+            self.run_standalone()
+            
+        self.output().open('w').close()
+        
 
 if __name__ == "__main__":
     luigi.run([
