5f2ba6b0de154237c73b2b53a1c3eb27e740a230
Zenon Evans <zenon.evans@hdfgroup.org>
2017-12-01 13:15:26 -0600

    test

--- pipeline/steps/readbucketdata.py
+++ pipeline/steps/readbucketdata.py
@@ -13,13 +13,13 @@ class ReadBucketData(ReadData):
 
     datafile = luigi.Parameter(default=STEP)
 
-    def __init__(self, csv_file, csv_file_keys):
+    def __init__(self):
         # self.task_id = ReadData.task_id
         # super(ReadBucketData, self).__init__('purchase_buckets.csv', BucketConfig().bucket_keys.split(","))
 
-        ReadData.__init__(self)
-        self.csv_file = csv_file
-        self.csv_file_keys = csv_file_keys
+        ReadData.__init__(self, 'purchase_buckets.csv', BucketConfig().bucket_keys.split(","))
+        # self.csv_file = csv_file
+        # self.csv_file_keys = csv_file_keys
 
     @staticmethod
     def requires():
