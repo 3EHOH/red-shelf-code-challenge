25c232dbbeb791f11b326e51896a4da11aac5ba0
Zenon Evans <zenon.evans@hdfgroup.org>
2017-12-01 12:11:31 -0600

    undo

--- pipeline/steps/readbucketdata.py
+++ pipeline/steps/readbucketdata.py
@@ -13,10 +13,38 @@ class ReadBucketData(ReadData):
 
     datafile = luigi.Parameter(default=STEP)
 
-    def __init__(self):
-        #ReadData.__init__(self, BucketConfig(), BucketConfig().bucket_keys.split(","))
-        #super(ReadBucketData, self).__init__(BucketConfig(), BucketConfig().bucket_keys.split(","))
-        super().__init__(BucketConfig(), BucketConfig().bucket_keys.split(","))
+    def __init__(self, csv_file, csv_file_keys):
+        self.csv_file = csv_file
+        self.csv_file_keys = csv_file_keys
+
+    @staticmethod
+    def requires():
+        return [PreflightCheck()]
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(PathConfig().target_path, self.datafile))
+
+    def run(self):
+        purchase_buckets_file = self.csv_file
+        bucket_keys = self.csv_file_key
+        bucket_data = []
+
+        with open(purchase_buckets_file) as csvfile:
+            bucket_reader = csv.reader(csvfile)
+            for row in bucket_reader:
+                bucket_record = {}
+                for i in range(len(bucket_keys)):
+                    bucket_record[bucket_keys[i]] = row[i]
+                bucket_data.append(bucket_record)
+
+        with self.output().open('w') as out_file:
+            out_file.write(json.dumps(bucket_data))
+
+
+    # def __init__(self):
+    #     #ReadData.__init__(self, BucketConfig(), BucketConfig().bucket_keys.split(","))
+    #     #super(ReadBucketData, self).__init__(BucketConfig(), BucketConfig().bucket_keys.split(","))
+    #     super().__init__(BucketConfig(), BucketConfig().bucket_keys.split(","))
 
     # @staticmethod
     # def requires():
