5daefa0eed816f11c57a379b67e2be50b45bca52
Zenon Evans <zenon.evans@hdfgroup.org>
2017-12-01 14:10:28 -0600

    purchase_buckets

--- pipeline/steps/readbucketdata.py
+++ pipeline/steps/readbucketdata.py
@@ -46,8 +46,8 @@ class ReadBucketData(ReadData):
         # self.task_id = ReadData.task_id
         # super(ReadBucketData, self).__init__('purchase_buckets.csv', BucketConfig().bucket_keys.split(","))
 
-        ReadData.__init__(self, BucketConfig().bucket_data, BucketConfig().bucket_keys.split(","))
-        self.csv_file = BucketConfig().bucket_data
+        ReadData.__init__(self, BucketConfig().purchase_buckets, BucketConfig().bucket_keys.split(","))
+        self.csv_file = BucketConfig().purchase_buckets
         self.csv_file_keys = BucketConfig().bucket_keys.split(",")
         # self.csv_file = csv_file
         # self.csv_file_keys = csv_file_keys
