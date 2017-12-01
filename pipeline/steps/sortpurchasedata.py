f8e91806388e53ae9ef9bae010d86459465021cf
Zenon Evans <zenon.evans@hdfgroup.org>
2017-12-01 16:44:29 -0600

    test'

--- pipeline/steps/sortpurchasedata.py
+++ pipeline/steps/sortpurchasedata.py
@@ -70,24 +70,28 @@ class SortPurchaseData(luigi.Task):
                     None)
                 if matched_bucket is not None:
                     matched_bucket['purchases'].append(record_values)
-            #
-            # elif next((bucket for bucket in bucket_data
-            #            if record['price'] == bucket['price']), None) is not None:
-            #
-            #     bucket_name_match = self.mock_bucket_name(None, record['price'])
-            #
-            #     matched_bucket = next((bucket for bucket in output_buckets if bucket['bucket'].lower() == bucket_name_match.lower()), None)
-            #     if matched_bucket is not None:
-            #         matched_bucket['purchases'].append(record_values)
-            #
-            # elif next((bucket for bucket in bucket_data
-            #            if record_duration_lc == bucket['duration'].lower()), None) is not None:
-            #
-            #     bucket_name_match = self.mock_bucket_name(None, None, record['duration'])
-            #
-            #     matched_bucket = next((bucket for bucket in output_buckets if bucket['bucket'].lower() == bucket_name_match.lower()), None)
-            #     if matched_bucket is not None:
-            #         matched_bucket['purchases'].append(record_values)
+
+            elif next((bucket for bucket in bucket_data
+                       if record['price'] == bucket['price']), None) is not None:
+
+                bucket_name_match = self.mock_bucket_name(None, record['price'])
+
+                matched_bucket = next(
+                    (bucket for bucket in output_buckets if bucket['bucket'].lower() == bucket_name_match.lower()),
+                    None)
+                if matched_bucket is not None:
+                    matched_bucket['purchases'].append(record_values)
+
+            elif next((bucket for bucket in bucket_data
+                       if record_duration_lc == bucket['duration'].lower()), None) is not None:
+
+                bucket_name_match = self.mock_bucket_name(None, None, record['duration'])
+
+                matched_bucket = next(
+                    (bucket for bucket in output_buckets if bucket['bucket'].lower() == bucket_name_match.lower()),
+                    None)
+                if matched_bucket is not None:
+                    matched_bucket['purchases'].append(record_values)
 
             else:
 
