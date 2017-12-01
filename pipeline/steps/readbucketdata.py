eec2852bda1ac95cbf02bcf515db7bb09c3ff983
Zenon Evans <zenon.evans@hdfgroup.org>
2017-12-01 13:32:14 -0600

    test

--- pipeline/steps/readbucketdata.py
+++ pipeline/steps/readbucketdata.py
@@ -52,28 +52,28 @@ class ReadBucketData(ReadData):
         # self.csv_file = csv_file
         # self.csv_file_keys = csv_file_keys
 
-    @staticmethod
-    def requires():
-        return [PreflightCheck()]
-
-    def output(self):
-        return luigi.LocalTarget(os.path.join(PathConfig().target_path, self.datafile))
-
-    def run(self):
-        purchase_buckets_file = self.csv_file
-        bucket_keys = self.csv_file_keys
-        bucket_data = []
-
-        with open(purchase_buckets_file) as csvfile:
-            bucket_reader = csv.reader(csvfile)
-            for row in bucket_reader:
-                bucket_record = {}
-                for i in range(len(bucket_keys)):
-                    bucket_record[bucket_keys[i]] = row[i]
-                bucket_data.append(bucket_record)
-
-        with self.output().open('w') as out_file:
-            out_file.write(json.dumps(bucket_data))
+    # @staticmethod
+    # def requires():
+    #     return [PreflightCheck()]
+    #
+    # def output(self):
+    #     return luigi.LocalTarget(os.path.join(PathConfig().target_path, self.datafile))
+    #
+    # def run(self):
+    #     purchase_buckets_file = self.csv_file
+    #     bucket_keys = self.csv_file_keys
+    #     bucket_data = []
+    #
+    #     with open(purchase_buckets_file) as csvfile:
+    #         bucket_reader = csv.reader(csvfile)
+    #         for row in bucket_reader:
+    #             bucket_record = {}
+    #             for i in range(len(bucket_keys)):
+    #                 bucket_record[bucket_keys[i]] = row[i]
+    #             bucket_data.append(bucket_record)
+    #
+    #     with self.output().open('w') as out_file:
+    #         out_file.write(json.dumps(bucket_data))
 
 
     # def __init__(self):
