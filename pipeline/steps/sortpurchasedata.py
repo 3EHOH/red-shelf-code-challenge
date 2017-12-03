0d3a59ec6602b216862adc0091da97662c024e8a
Zenon Evans <zenon.evans@hdfgroup.org>
2017-12-03 10:27:48 -0600

    test

--- pipeline/steps/sortpurchasedata.py
+++ pipeline/steps/sortpurchasedata.py
@@ -20,6 +20,9 @@ class SortPurchaseData(luigi.Task):
             record_publisher_lc = record['publisher'].lower()
             record_duration_lc = record['duration'].lower()
 
+            if record_publisher_lc == "scipub":
+                print("SCI PUB PRINT")
+
             if next((bucket for bucket in bucket_data
                      if bucket['publisher'].lower() == record_publisher_lc
                      and record['price'] == bucket['price']
@@ -59,7 +62,7 @@ class SortPurchaseData(luigi.Task):
                     matched_bucket['purchases'].append(record_values)
 
             elif next((bucket for bucket in bucket_data
-                       if bucket['publisher'].lower() == "scipub"
+                       if bucket['publisher'].lower() == record_publisher_lc
                        and record['price'] == '*'
                        and record_duration_lc == '*'), None) is not None:
 
