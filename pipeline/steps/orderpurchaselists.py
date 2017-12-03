32403118d3bbbc8b684e23a399f86f97a05eba5b
Zenon Evans <zenon.evans@hdfgroup.org>
2017-12-03 13:47:31 -0600

    .

--- pipeline/steps/orderpurchaselists.py
+++ pipeline/steps/orderpurchaselists.py
@@ -15,6 +15,7 @@ class OrderPurchaseLists(luigi.Task):
     def order_purchase_lists(purchase_data):
 
         for bucket in purchase_data:
+            print(bucket['purchases'])
             sorted_lists = sorted(bucket['purchases'], lambda x: int(x.split(",")[0]))
             bucket['purchases'] = sorted_lists
 
